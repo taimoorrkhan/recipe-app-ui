d10f93e1d0eb91287242edaf4879ae0ee80ecd79
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2024-02-04 14:29:12 +0500

    added List screen

---
+++ src/screens/RecipeListScreen.js
@@ -0,0 +1,39 @@
+import { StyleSheet, View, Text, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+import Header from "../components/Header";
+import SearchFilter from "../components/SearchFilter";
+import CategoriesFilter from "../components/CategoriesFilter";
+import RecipeCard from "../components/RecipeCard";
+
+const RecipeListScreen = () => {
+	return (
+		<SafeAreaView style={{ flex: 1, marginHorizontal: 16,marginTop:18 }}>
+			{/* render header */}
+			<Header headerText={"Hi, John "} headerIcon={"bell-o"} />
+
+			{/* Search Filter */}
+			<SearchFilter icon="search" placeholder={"enter your fav recipe"} />
+
+			{/* Categories filter */}
+
+			<View style={{ marginTop: 15 }}>
+				<Text style={{ fontSize: 22, fontWeight: "bold" }}>Categories</Text>
+				{/* Categories list */}
+				<CategoriesFilter />
+			</View>
+
+			{/* Recipe List Filter */}
+
+			<View style={{ marginTop: 10, flex: 1,marginVertical:25  }}>
+				<Text style={{ fontSize: 22, fontWeight: "bold",marginBottom:10 }}>Recipes</Text>
+				{/* Recipes list */}
+
+				<RecipeCard />
+			</View>
+		</SafeAreaView>
+	);
+};
+
+export default RecipeListScreen;
+
+const styles = StyleSheet.create({});
